8abc91c0162367c9edeca2578c4fb4fec0b0a139
ldakhoa <ldakhoa1308@gmail.com>
2022-06-20 15:57:06 +0700

    Initial Commit

---
+++ SoundyControl/SoundyControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoundyControlApp.swift
+//  SoundyControl
+//
+//  Created by Khoa Le on 20/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct SoundyControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
